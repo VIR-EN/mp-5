21c9f666f98923687f02e517d43babdb996cdd20
Viren Arora <virenarora@crc-dot1x-nat-10-239-59-145.bu.edu>
2025-11-18 19:33:05 -0500

    three

--- lib/createShortLink.ts
+++ lib/createShortLink.ts
@@ -7,11 +7,37 @@ export default async function createShortLink(
     url: string
 ): Promise<{ success: boolean; message: string }> {
     try {
+
+        // Check for invalid alias characters
+        if (encodeURIComponent(alias) !== alias) {      // built in constructor that changes unsafe spaces or symbols in link to browser safe characters
+            return {
+                success: false,                 // this ensures we dont use an invalid alias, running into issues later
+                message: "Invalid alias"
+            };
+        }
+
         // Validate URL format
         try {
             new URL(url); // uses the JS url constructer to check the url syntax and confirms that its valid
         } catch {
             return { success: false, message: "Invalid URL format." }; // if it fails it alerts the user and thors and error before faulty url can be inserted into the db
+        }
+
+        try {
+            const res = await fetch(url, { method: "HEAD" });  // feches the metadata from the head, lightweight staus code check, if the response code is valid
+            if (res.status < 200 || res.status >= 500) {
+
+                return {
+                    success: false,                         // if the response code is outside the validity paramaters, the user is alerted
+                    message: "Invalid URL: Re-check your URL "
+                };
+            }
+        } catch {
+
+            return {
+                success: false,
+                message: "Invalid URL: Could not verify target site.",
+            };
         }
 
         const links = await getCollection("links");
